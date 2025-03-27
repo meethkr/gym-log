e5fc50e1e562d69cf9a95910275760f45095c6ec
Harikrishnan <meethkr@gmail.com>
2025-03-27 23:16:25 +0530

    Boilerplate code, gitignore

---
+++ src/gym_log/app.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+from datetime import datetime
+from gym_log.utils.helpers import format_date 
+
+print("This is a gym log in the making")
+print(format_date(datetime.now()))
---
+++ src/gym_log/utils/helpers.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def format_date(date):
+    return date.strftime("%Y-%m-%d")
